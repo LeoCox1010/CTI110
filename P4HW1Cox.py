a6b60545082ed02b4fa7a9b64b76946803d0ecb9
LeoCox1010 <coxm2830@student.faytechcc.edu>
2022-11-01 14:06:49 -0400

    Update P4HW1Cox.py
    min, max. total, and average

--- P4HW1Cox.py
+++ P4HW1Cox.py
@@ -3,4 +3,28 @@
 #Leo Cox
 #11/1/22
 
-#Ask for 6 grades 
+#Ask for 6 grades for 6 modules 
+
+grades = []
+
+
+for grade in range(6):
+    grade = int(input("Enter grade: "))
+    grades.append(grade)
+
+print("The grades are: ", grades)
+
+# max and min
+
+print ("Highest grade: ", max(grades))
+
+print ("Lowest grade: ", min(grades))
+
+#total and average
+
+total = sum(grades)
+count = len(grades)
+average = total/count
+
+print ("Total is: ", total)
+print("Average is: ", average)
